86cd2f68983329eb56d7102094655449612465af
olgamati <olgailjina88@gmail.com>
2022-08-31 21:56:06 +0200

    Added the Dictionary component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <div className="container">
+        <header className="App-header">Hello</header>
+        <main>
+          <Dictionary />
+        </main>
+        <small>
+          This project is coded by{" "}
+          <a
+            href="https://olga-plotnikova.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Olga Plotnikova
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/olgamati/mati-dictionary-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </small>
+      </div>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,5 @@
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  return <div className="Dictionary">"Search"</div>;
+}
